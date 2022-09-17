403a1a292ff7ce74c559b1d813aa16f1fab14eb0
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-17 23:25:25 +0600

    add demo SSLcommerz payment

--- My_Ecom_Project/settings.py
+++ My_Ecom_Project/settings.py
@@ -1,4 +1,6 @@
+from ctypes import cast
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -10,10 +12,10 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c_oxa!3+-u_b-mrb)_389c+w4owur-4w3wccb@ks&n48wt5oz7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 

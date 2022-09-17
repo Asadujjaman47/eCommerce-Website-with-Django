561c8629b2cb27ffbfbde0b270cd23a0d4d31405
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-17 20:05:56 +0600

    setup SSLcommerz, fix python version problem

--- App_Payment/views.py
+++ App_Payment/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, redirect
 from django.contrib import messages
 
 # models and forms
@@ -8,6 +8,13 @@ from App_Payment.forms import BillingForm
 
 #
 from django.contrib.auth.decorators import login_required
+
+
+# for payment
+import requests
+from sslcommerz_python.payment import SSLCSession
+from decimal import Decimal
+import socket
 
 # Create your views here.
 
@@ -33,3 +40,18 @@ def checkout(request):
     order_total = order_qs[0].get_totals()
 
     return render(request, 'App_Payment/checkout.html', context={'form': form, 'order_items': order_items, 'order_total': order_total, 'saved_address': saved_address})
+
+
+# payment view:
+@login_required
+def payment(request):
+    saved_address = BillingAddress.objects.get_or_create(user=request.user)
+    if not saved_address[0].is_fully_filled():
+        messages.info(request, f"Please complete shipping address!")
+        return redirect("App_Payment:checkout")
+
+    if not request.user.profile.is_fully_filled():
+        messages.info(request, f"Please complete profile details!")
+        return redirect("App_Login:profile")
+
+    return render(request, "App_Payment/payment.html", context={})
